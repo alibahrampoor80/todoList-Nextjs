da57cc83f0de46b741f259ae42a0f08ecceb950b
Ali Bahrampoor <ali.bahrampoor1380@gmail.com>
2023-01-24 11:36:41 +0330

    update project - new style for detail page

--- src/pages/todos/[todoId].js
+++ src/pages/todos/[todoId].js
@@ -1,12 +1,20 @@
-
-import {getOneTodo} from "../api/todos/[todoId]";
+import {getOneTodo} from "@/api/todos/[todoId]";
+import Layout from "@/containers/Layout";
 
 const TodoPage = ({todo}) => {
-    return <>
-        <h1>Todo detail page</h1>
-        <h2>title: {todo.title}</h2>
-        <p>description: {todo.description}</p>
 
+    return <>
+
+        <Layout>
+            <div className="container p-2 xl:max-w-screen-xl mx-auto">
+                <div className="w-full max-w-screen-md mx-auto text-center bg-white p-2 md:p-4 rounded-xl">
+                    <h1 className={'text-4xl mb-4'}>Todo detail page</h1>
+                    <h2 className={'font-bold'}>title: <span className={'font-normal'}>{todo.title}</span></h2>
+                    <p className={'font-bold'}>description: <span className={'font-normal'}>{todo.description}</span></p>
+                    <span className={'font-bold'}>Complete Task: <span className={'font-normal'}>{JSON.stringify(todo.isCompleted)}</span></span>
+                </div>
+            </div>
+        </Layout>
     </>
 }
 export default TodoPage
